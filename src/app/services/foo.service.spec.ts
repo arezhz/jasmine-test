62c5b21c6c15557741f38aea680d0dd3a4b7a11a
arezhz <arezhz@gmail.com>
2023-04-28 00:22:37 +0330

    foo service

--- src/app/services/foo.service.spec.ts
+++ src/app/services/foo.service.spec.ts
@@ -6,7 +6,7 @@ describe('FooService', () => {
   let service: FooService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    // TestBed.configureTestingModule({});
     service = TestBed.inject(FooService);
   });
 
